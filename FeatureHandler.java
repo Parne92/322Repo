a24e5eed7f7f0d43b1d0abd95b637f61b83d96e6
Patrick Tung <83930287+Patrickrtu@users.noreply.github.com>
2022-04-13 14:01:03 -0600

    Fully commented FeatureHandler class

--- FeatureHandler.java
+++ FeatureHandler.java
@@ -1,21 +1,37 @@
 import java.util.ArrayList;
 
 /**
+ * The FeatureHandler is basically the backend of the app that keeps track of a
+ * list of Users (Observers) that depend on it. The FeatureHandler object
+ * will notify its dependents automatically of any state changes by
+ * calling one of their methods.
+ * 
  * @author Patrick Tung
  */
 public class FeatureHandler implements Observable {
 
+    // Declare and initialize an ArrayList to keep track of all observers
     private ArrayList<Observer> users = new ArrayList<>();
+    // field that keeps track of door lock state
     private boolean doorIsLocked;
+    // field that keeps track of security camera state
     private boolean camFunctional;
+    // field that keeps track of indoor lighting state
     private boolean lightsOn;
 
+    /**
+     * Add a dependent Observer to the ArrayList. Grant user access.
+     */
     @Override
     public void registerUser(Observer newUser) {
         users.add(newUser);
         newUser.setRegistrationStatus(true);
     }
 
+    /**
+     * Print out message. Remove a dependent Observer from the ArrayList. Remove
+     * user access.
+     */
     @Override
     public void removeUser(Observer user) {
         int userIndex = users.indexOf(user);
@@ -24,6 +40,9 @@ public class FeatureHandler implements Observable {
         user.setRegistrationStatus(false);
     }
 
+    /**
+     * Notify all Observers of any state changes.
+     */
     @Override
     public void notifyUsers() {
         for (Observer user : users) {
@@ -31,6 +50,13 @@ public class FeatureHandler implements Observable {
         }
     }
 
+    /**
+     * Change door lock state.
+     * 
+     * @param lockDoor boolean that will set lock state. Set {@code lockDoor} to
+     *                 {@code true} and
+     *                 the door is locked.
+     */
     public void lockDoor(boolean lockDoor) {
         if (lockDoor) {
             this.doorIsLocked = true;
@@ -40,6 +66,13 @@ public class FeatureHandler implements Observable {
         notifyUsers();
     }
 
+    /**
+     * Change Security Camera state.
+     * 
+     * @param activateCam boolean that will set Security Camera state. Set
+     *                    {@code activateCam} to {@code true} and the camera is
+     *                    turned on.
+     */
     public void activateCam(boolean activateCam) {
         if (activateCam) {
             this.camFunctional = true;
@@ -49,6 +82,13 @@ public class FeatureHandler implements Observable {
         notifyUsers();
     }
 
+    /**
+     * Change Indoor Lighting state.
+     * 
+     * @param turnOnLights boolean that will set Indoor Lighting state. Set
+     *                     {@code turnOnLights} to {@code true} and the lights will
+     *                     turn on.
+     */
     public void turnOnLights(boolean turnOnLights) {
         if (turnOnLights) {
             this.lightsOn = true;
--- GroupProjectTest.java
+++ GroupProjectTest.java
@@ -1,6 +1,24 @@
+/**
+ * The home security mobile app will connect to the backend and prompt user to
+ * create a new account.
+ * Upon account creation, the app will offer users a set of options that control
+ * various features within the app.
+ * After user interacts with the app, backend will automatically update all
+ * users with the current state of the sensors.
+ * When a user deletes their account, they will no longer have access to the
+ * app.
+ * 
+ * @author Patrick Tung
+ */
 public class GroupProjectTest {
+
+    /**
+     * Main method that instantiate the app. Everything else is hidden.
+     * 
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
-        //TODO comment out everything 
+        // TODO comment out everything
 
         FeatureHandler backend = new FeatureHandler();
 
